1f21779c5f63b77ea0686822f972e1005625caa7
Fluffy-Bean <michal-gdula@protonmail.com>
2022-06-16 15:39:28 +0100

    Switched gallery to WEBP and adjusted page contents

--- index.php
+++ index.php
@@ -9,12 +9,18 @@
 	<link rel="icon" type="image/x-icon" href="images/fluffy.png">
 	<!-- Style -->
 	<link rel="stylesheet" href="stylesheet.css">
+	<!-- Fonts -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&amp;display=swap">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@600&amp;display=swap">
 </head>
 <body>
 
   <nav>
-		<img src="images/fluffy.png">
-		<div>
+		<div class="nav-logo">
+			<img src="images/fluffy.png">
+			<h2><span class="fluffy-colour">Fluffy's hell</span></h2>
+		</div>
+		<div class="nav-links">
 			<a href="https://twitter.com/fluffybeanUwU">Twitter</a>
 			<a href="http://t.me/Fluffy_Bean">Telegram</a>
 			<a href="https://github.com/Fluffy-Bean">Github</a>
@@ -28,41 +34,42 @@
 			<p>This is supposed to be a descriptive intro line, but I don't feel like writing this</p>
 		</section>
 
-		<section id="about">
-			<h2>About me</h2>
-			<p>Hi! I'm <span class="fluffyColour">Fluffy Bean</span>. I'm a 17 year old who found himself on the internet one day and became a furry! (And a developer)</p>
+		<section id="summary" class="justify-text">
+			<h2>Summary</h2>
+			<p>Hi! I'm <span class="fluffy-colour">Fluffy Bean</span>. I'm a 17 year old who found himself on the internet one day and became a furry! (And a developer)</p>
 		</section>
 
-		<section id="interests">
+		<section id="interests" class="justify-text">
 			<h2>My interests</h2>
 			<p>Appart from the obvious, I am interested in computers, specifically the programming aspect of them and the freedom it gives me to express myself</p>
-			<p>I also enjoy using Linux (Arch btw) which gives me access to do things such as custormise my entire system!</p>
-			<p>Another thing I enjoy doing is playing games, some of my favroute tiles are, The Witcher 3, Minecraft, Beat Saber and Spin Rhythm XD</p>
+			<p>I also enjoy using Linux (Arch btw) which gives me access to do things such as customise my entire system!</p>
+			<p>Another thing I enjoy doing is playing games, some of my favourite tiles are, The Witcher 3, Minecraft, Beat Saber and Spin Rhythm XD</p>
 		</section>
 
-		<section id="experiance">
-			<h2>Experiance in programming</h2>
-			<p>I have a decently wide range of knowlage on computers and programming </p>
+		<section id="experience" class="justify-text">
+			<h2>Experience in programming</h2>
+			<p>I have a decently wide range of knowledge on computers and programming </p>
 			<p>Since I was young I delved into computers head first, and breaking a lot on the way. I started with Scratch, then progressed to Python in Secondary School where I really started my programming adventure!</p>
 			<p>Since then I tried many programming languages and programming-related stuff, I currently know Python, HTML and CSS but am also learning PHP and C#</p>
-			<p>I also got into Linux since, the start was rough with me breaking a bunch of stuff. But since then I've been using Arch (btw) and happily configuring Rofi, Polybar, fish and even theming Discord, Spotify and Firefox!</p>
+			<p>I also got into Linux since the start was rough with me breaking a bunch of stuff. But since then I've been using Arch (btw) and happily configuring Rofi, Polybar, fish and even theming Discord, Spotify and Firefox!</p>
+			<p>These stats come from my GitHub profiles public repositories (private ones are not counted)</p>
 			<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Fluffy-Bean&layout=compact">
 		</section>
 
-		<section>
+		<section class="justify-text">
 			<h2>Special Thanks</h2>
-			<p>To <span class="jeetixColour">Jeetix</span> for helping me with learning how to make websites and keeping the original site up and running!</p>
-			<p>And to <span class="cartyColour">Carty</span> for teaching me how to run servers and the networking required to keep sites online!</p>
+			<p>To <span class="jeetix-colour">Jeetix</span> for helping me with learning how to make websites and keeping the original site up and running!</p>
+			<p>And to <span class="carty-colour">Carty</span> for teaching me how to run servers and the networking required to keep sites online!</p>
 		</section>
 
 		<section id="gallery">
 			<h2>Random pictures lol</h2>
-			<img src="gallery/vrc_christmas_meetup_2021.webp" class="gallery-image">
+			<img src="gallery/fluffy_VRchat-meet-2021.webp" class="gallery-image">
 			<div class="gallery" draggable="false">
 				<?php
 				$images = glob("gallery/*.{jpg,jpeg,png,webp,gif}", GLOB_BRACE);
 				foreach ($images as $i) {
-					printf('<img src="gallery/%s" draggable="false"/>',
+					printf('<img src="gallery/%s" draggable="false" loading="lazy"/>',
 						basename($i)
 					);
 				}
@@ -73,7 +80,7 @@
 
 		<section id="refsheet">
 			<h2>Refsheet</h2>
-			<img src="images/ref.png" alt="Refsheet containing an orange anthro Maned Wolf.">
+			<img src="images/ref.png" alt="Ref Sheet containing an orange anthro Maned Wolf." loading="lazy">
 			<h3>Bandana</h3>
 			<p>#FF7700 - #FFDC00 - #00D621 - #0088FF</p>
 			<h3>Fur</h3>
@@ -82,18 +89,18 @@
 			<p>#febbec - #c859c9</p>
 		</section>
 
-		<section id="jokes">
+		<section id="jokes" class="justify-text">
 			<h2>Funny jokes!</h2>
 			<h3>Cow joke</h3>
 			<p>What did the cow say to the other cow that took up all the space? "Mooooove over"!</p>
 			<h3>Eel joke</h3>
-			<p>Why can't electric eels get depresed? Because they're always positive!</p>
+			<p>Why can't electric eels get depressed? Because they're always positive!</p>
 		</section>
 
-		<section id="pc">
+		<section id="pc" class="justify-text">
 			<h2>System configuration</h2>
 			<h3>Operating system</h3>
-			<img src="images/sussywussy.png" alt="Screenshot of Linux desktop enviroment">
+			<img src="images/sussywussy.png" alt="Screenshot of Linux desktop environment" loading="lazy">
 			<p>My main operating system is Arch Linux, in this screenshot you can see a "rice" of Rofi and Polybar! Which you can see <a href="https://github.com/Fluffy-Bean/dots">here</a>!</p>
 			<p>My secondary system is Windows 10 Pro just for VR</p>
 
@@ -105,17 +112,19 @@
 			<p>For storage I am using a Hitachi Deskstar that has 2TBs of storage for my games and larger files. For my boot SSD I am using a Samsung 980 1TB M.2 NVMe.</p>
 
 			<h3>Equipment</h3>
-			<p>My keyboard is a Razer Blackwiddow X Chroma that has been through too much. To go with that I have a Logitech g502 mouse with a Hyena Agenda mousemat.</p>
+			<p>My keyboard is a Razer Blackwiddow X Chroma that has been through too much. To go with that I have a Logitech g502 mouse with a Hyena Agenda mouse mat.</p>
 			<p>For my monitor I have a BenQ MOBIUZ EX2510. For VR I have a normal CV1 with 2 cameras.</p>
+			<p>Headphones are very important to me, as I use them for hours at a time everyday and listen to a lot of music, so I got myself Steelseries Arctis 3 (only to be very dissapointed with the mic and build quality). I also enjoy playing some games with a controller so I have a modded Xbox One S controller.</p>
 
 			<h3>Server</h3>
 			<p>My server is a simple Zotac ZBOX-CI527.</p>
 			<p>It still has the base 8GBs of ram and i5-7200U CPU. But I upgraded it with a 120GB SSD.</p>
-			<p>And it is running a Proxmox server with many many LXC containters because why not :3</p>
+			<p>And it is running a Proxmox server with many many LXC containers because why not :3</p>
 		</section>
 
-		<section id="spotify">
-			<h2>Mah Spotify playlist!</h2>
+		<section id="spotify" class="justify-text">
+			<h2>Mah Music taste!</h2>
+			<p>Here are some playlists I made with some music I enjoy listening to. Though I missed many many songs.</p>
 			<iframe src="https://open.spotify.com/embed/playlist/5e4iqBdioVN56hz1KCYWNi?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
 			<iframe src="https://open.spotify.com/embed/playlist/37vWcad8w7GBvNIZUkDxEi?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
 		</section>
@@ -129,8 +138,8 @@
 		</div>
 		<div>
 			<h3>Page status</h3>
-			<p>Last updated 15 Jun 2022</p>
-			<p>Made and designed by Fluffy</p>
+			<p>Last updated 16 Jun 2022</p>
+			<p>Made and designed by <span class="fluffy-colour">Fluffy</span></p>
 		</div>
 		<div>
 			<h3>Other works</h3>
