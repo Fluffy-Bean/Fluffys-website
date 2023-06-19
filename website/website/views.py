43665fb93021632d72f837befbb7cfcb16f4c88f
Fluffy-Bean <michal-gdula@protonmail.com>
2023-06-19 16:48:32 +0000

    Yeet docker!!!!

---
+++ website/website/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "views/index.html")
+
+
+def refsheet(request):
+    return render(request, "views/refsheet.html")
